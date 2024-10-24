bf74bca6c14a4572ad39f966acee06cfb2072ee0
Ripon-Mardy <mardyripon10@gmail.com>
2024-10-24 13:34:43 +0600

    first commit

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <h1 className='text-7xl text-center -skew-y-6 font-bold mt-6'>Danish</h1>
+    <h1 className='text-7xl text-center -skew-y-6 font-bold mt-6'>Murmu</h1>
+    </>
+  )
+}
+
+export default Header
